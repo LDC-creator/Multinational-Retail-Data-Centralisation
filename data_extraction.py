e9cef1479eaeca0ba682dd97aeaa5306b16f16f3
LDC-creator <131806469+LDC-creator@users.noreply.github.com>
2024-04-06 20:33:30 +0100

    creating engne and readable table names from database

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,6 @@
+from sqlalchemy import create_engine, MetaData
 import yaml
-from sqlalchemy import create_engine
+import pandas as pd
 
 class DataExtractor:
     def read_db_creds(self, file_path):
@@ -23,7 +24,6 @@ class DataExtractor:
             print(f"Error parsing YAML file: {e}")
             return {}
 
-
     def init_db_engine(self, file_path):
         creds = self.read_db_creds(file_path)
         RDS_HOST = creds['RDS_HOST']
@@ -33,27 +33,60 @@ class DataExtractor:
         RDS_PORT = creds['RDS_PORT']
         engine = create_engine(f"postgresql://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}")
         return engine
-    
-    def main(self):
-        file_path = "db_creds.yaml"
+
+    def list_db_tables(self, file_path):
+        # Establish a connection to the database
         engine = self.init_db_engine(file_path)
-        print(engine)
-if __name__ == "__main__":
-    DataExtractor().main()
-
         
+        # Create a MetaData object
+        meta = MetaData()
+        
+        # Reflect metadata from the database
+        meta.reflect(bind=engine)
+        
+        # Get the list of table names
+        tables = meta.tables.keys()
+        
+        return tables
 
+    def read_rds_table(self, connector, table_name):
+        """
+        Extracts the specified database table to a pandas DataFrame.
 
+        Args:
+            connector (DatabaseConnector): An instance of DatabaseConnector class.
+            table_name (str): Name of the database table to extract.
 
+        Returns:
+            pandas.DataFrame: DataFrame containing the extracted data.
+        """
+        # Establish a connection to the database using DatabaseConnector
+        engine = connector.get_engine()
+        
+        # Read the specified table into a DataFrame
+        query = f"SELECT * FROM {table_name}"
+        df = pd.read_sql(query, engine)
+        
+        return df
 
+# Create an instance of DataExtractor
+extractor = DataExtractor()
 
+# Path to the db_creds.yaml file
+file_path = '/Users/User/MRDC/Multinational-Retail-Data-Centralisation/db_creds.yaml'
 
+# List tables from the database and print the result
+result_tables = extractor.list_db_tables(file_path)
+print("Tables in the database:")
+for table in result_tables:
+    print(table)
 
+# Assuming you have an instance of DatabaseConnector named 'connector'
+# and you have the table name containing user data
+table_name = "legacy_users"
 
-# # Example usage:
-# file_path = "/Users/User/MRDC/Multinational-Retail-Data-Centralisation/db_creds.yaml"
-# extractor = DataExtractor()
-# creds = extractor.read_db_creds(file_path)
-# print(creds)
+# Call the read_rds_table method to extract the specified table
+data_frame = extractor.read_rds_table(connector, table_name)
 
-
+# Print the DataFrame
+print(data_frame)
